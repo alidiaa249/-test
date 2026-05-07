d0007171298c58f3e7b0027e2271725f9ded3ba0
alidiaa249 <alidiaa249@gmail.com>
2026-05-07 13:50:12 +0300

    Add files via upload

---
+++ Nova_Team-main/script.js
@@ -0,0 +1,19 @@
+window.addEventListener("DOMContentLoaded" , () => {
+let links = document.querySelectorAll("nav a");
+let currentPage = window.location.pathname.split("/").pop();
+
+if (currentPage === "") {
+  currentPage = "index.html";
+}
+links.forEach(link => {
+  let linkPage = link.getAttribute("href");
+
+  if (linkPage === currentPage) {
+    link.classList.add("nav-active");
+  }
+});
+})
+
+
+
+
